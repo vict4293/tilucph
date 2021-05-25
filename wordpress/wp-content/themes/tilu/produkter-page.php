e6474fdd7074da08f36035206eda6bcccc9c9082
Victoria Løkke <victorialoekke@MacBook-Pro-tilhrende-Victoria.local>
2021-05-25 11:24:32 +0200

    indsætter produkterne første forsøg

---
+++ wordpress/wp-content/themes/tilu/produkter-page.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<nav id="filtrering">
+				<button data-genre="alle">Vis alle</button>
+			</nav>
+
+			<section id="produkt-oversigt"></section>
+
+
+			<template>
+				<article>
+					<img src="" alt="" class="billede">
+					<p class="titel"></p>
+				</article>
+			</template>
+
+			<script>
+				let produkter = [];
+				const oversigt = document.querySelector("#produkt-oversigt");
+				const skabelon = document.querySelector("template");
+				let filterProdukt = "alle";
+
+				//Når alt content på siden er loaded sætter vi functionen start igang og henter json//
+				document.addEventListener("DOMContentLoaded", start);
+
+				function start() {
+					getJson();
+				}
+
+				//Henter data gennem WP rest API url med fetch funktion//
+				const url = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/podcast?per_page=100";
+
+				async function getJson() {
+					let response = await fetch(url);
+					produkter = await response.json();
+					visProdukter();
+				}
+
+				//Viser produkterne gennem et forEach loop//
+				function visProdukter() {
+					console.log(produkter);
+					produkter.forEach(produkt => {
+						const klon = skabelon.cloneNode(true).content;
+						klon.querySelector(".titel").textContent = produkt.title.rendered;
+						klon.querySelector(".billede").src = produkt.billede.guid;
+					})
+				}
+
+			</script>
+		</main>
+		<!-- #main -->
+	</div>
+	<!-- #primary -->
+
+	<?php
+get_footer();
