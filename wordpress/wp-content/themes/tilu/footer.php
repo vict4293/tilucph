55679587df1e9c17a16ae1f7975004192f81dadc
Victoria Løkke <victorialoekke@MacBook-Pro-tilhrende-Victoria.local>
2021-05-27 14:05:10 +0200

    footer

---
+++ wordpress/wp-content/themes/tilu/footer.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+?>
+
+	</div>
+	<!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+
+
+
+		<form>
+			<label for="email">TILMELD DIG</label>
+			<input id="email" name="email" type="email">
+			<button>INDSEND</button>
+		</form>
+
+		<div class="col">
+			<h3>KONTAKT</h3>
+			<p>kontakt@radioloud.dk
+				<br> +45 32 42 17 14
+				<br>
+				<br> <b> Programredaktion</b>
+				<br>Wildersgade 10B, 2. sal
+				<br>1408 København
+				<br>
+				<br> <b> Nyhedsredaktion</b>
+				<br>Vestergade 165D, 2. sal
+				<br>5700 Svendborg
+			</p>
+		</div>
+
+	</footer>
+	<!-- #colophon -->
+
+	</div>
+	<!-- #page -->
+
+	<?php wp_footer(); ?>
+
+		</body>
+
+		</html>
