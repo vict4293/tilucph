15bba7fa25390468344b10a12a2e1d8f407013b6
Victoria Løkke <victorialoekke@MacBook-Pro-tilhrende-Victoria.local>
2021-06-02 15:45:08 +0200

    kollektioner

--- wordpress/wp-content/themes/tilu/page-kollektioner.php
+++ wordpress/wp-content/themes/tilu/page-kollektioner.php
@@ -21,66 +21,87 @@ get_header();
 		<main id="main" class="site-main">
 
 			<nav id="filtrering">
-			<button data-produkt="alle">Alle</button>
+				<button data-genre="alle">Alle</button>
 			</nav>
 
 			<section id="kollektion-oversigt"></section>
 		</main>
 		<!-- #main -->
-		<script>
-			let kollektioner = [];
-			let categories;
-
-			const oversigt = document.querySelector("#kollektion-oversigt");
-			const skabelon = document.querySelector("template");
-
-			//Henter data gennem WP rest API url med fetch funktion/
-			const url = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/kollektion?per_page=100";
-			const caturl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/categories";
-
-			//Når alt content på siden er loaded sætter vi functionen start igang og henter json//
-			document.addEventListener("DOMContentLoaded", start);
-
-			function start() {
-				getJson();
-			}
-
-			async function getJson() {
-				const data = await fetch(url);
-				const catdata = await fetch(caturl);
-				kollektioner = await data.json();
-				categories = await catdata.json();
-				console.log(categories);
-				visKollektioner();
-				opretKnapper();
-			}
-
-			function opretKnapper() {
-				categories.forEach(cat => {
-					document.querySelector("#filtrering").innerHTML += `<button class="filter" data-kategori="${cat.id}">${cat.name}</button>`
-
-				})
-
-				addEventListenersToButtons();
-
-			}
-
-			//Viser kollektionerne gennem et forEach loop//
-			function visKollektioner() {
-				console.log(kollektioner);
-				kollektioner.forEach(kollektion => {
-					if (filterProdukt == "alle" || podcast.categories.includes(parseInt(filterPodcast))) {
-					const klon = skabelon.cloneNode(true).content;
-					klon.querySelector(".titel").textContent = kollektion.title.rendered;
-					klon.querySelector(".billede").src = kollektion.billede.guid;
-				})
-			)}
-
-		</script>
-
-
 	</div>
 	<!-- #primary -->
+
+
+	<script>
+		let kollektioner;
+		let categories;
+		let filterKollektion = "alle";
+
+		//Henter data gennem WP rest API url med fetch funktion//
+		const dburl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/kollektion?per_page=100";
+		const caturl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/categories";
+
+
+		//Når alt content på siden er loaded sætter vi functionen start igang og henter json//
+		document.addEventListener("DOMContentLoaded", start);
+
+		function start() {
+			getJson();
+		}
+
+		async function getJson() {
+			const data = await fetch(dburl);
+			const catdata = await fetch(caturl);
+			kollektioner = await data.json();
+			categories = await catdata.json();
+			console.log(categories);
+			visKollektioner();
+			opretKnapper();
+		}
+
+		function opretKnapper() {
+			categories.forEach(cat => {
+				document.querySelector("#filtrering").innerHTML += `<button class="filter" data-kollektion="${cat.id}">${cat.name}</button>`
+
+			})
+
+			addEventListenersToButtons();
+		}
+
+		function addEventListenersToButtons() {
+			document.querySelectorAll("#filtrering button").forEach(elm => {
+				elm.addEventListener("click", filtrering);
+			})
+		};
+
+		function filtrering() {
+			filterKollektion = this.dataset.kollektion;
+			visKollektioner();
+		}
+
+		//Viser produkterne gennem et forEach loop//
+		function visKollektioner() {
+			let temp = document.querySelector("template");
+			let container = document.querySelector("#kollektion-oversigt");
+			container.innerHTML = "";
+			kollektioner.forEach(kollektion => {
+				if (kollektion.categories.includes(parseInt(filterKollektion))) {
+					let klon = temp.cloneNode(true).content;
+
+					klon.querySelector(".titel").textContent = kollektion.title.rendered;
+
+					klon.querySelector("img").src = kollektion.billede[0].guid;
+
+					klon.querySelector("article").addEventListener("click", () => {
+						location.href = kollektion.link;
+					})
+					container.appendChild(klon);
+				}
+			})
+		}
+
+		getJson();
+
+	</script>
 
 	<?php
 get_footer();
