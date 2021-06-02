aa4101c8a618aa1304ef7ab1152be545efb42ca6
KatrinMaria <katrinmaria111@gmail.com>
2021-06-02 16:27:01 +0200

    kollektioner i stedet for kollektion i vis kollektion

--- wordpress/wp-content/themes/tilu/page-kollektioner.php
+++ wordpress/wp-content/themes/tilu/page-kollektioner.php
@@ -11,97 +11,97 @@
 
 get_header();
 ?>
-	<template>
-		<article class="kollektion">
-			<img src="" alt="" class="billede">
-			<h2 class="titel"></h2>
-		</article>
-	</template>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<template>
+    <article class="kollektion">
+        <img src="" alt="" class="billede">
+        <h2 class="titel"></h2>
+    </article>
+</template>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-			<nav id="filtrering">
-				<button data-genre="alle">Alle</button>
-			</nav>
+        <nav id="filtrering">
+            <button data-genre="alle">Alle</button>
+        </nav>
 
-			<section id="kollektion-oversigt"></section>
-		</main>
-		<!-- #main -->
-	</div>
-	<!-- #primary -->
-
-
-	<script>
-		let kollektioner;
-		let categories;
-		let filterKollektion = "alle";
-
-		//Henter data gennem WP rest API url med fetch funktion//
-		const dburl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/produkt?per_page=100";
-		const caturl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/categories";
+        <section id="kollektion-oversigt"></section>
+    </main>
+    <!-- #main -->
+</div>
+<!-- #primary -->
 
 
-		//Når alt content på siden er loaded sætter vi functionen start igang og henter json//
-		document.addEventListener("DOMContentLoaded", start);
+<script>
+    let kollektioner;
+    let categories;
+    let filterKollektion = "alle";
 
-		function start() {
-			getJson();
-		}
+    //Henter data gennem WP rest API url med fetch funktion//
+    const dburl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/produkt?per_page=100";
+    const caturl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/categories";
 
-		async function getJson() {
-			const data = await fetch(dburl);
-			const catdata = await fetch(caturl);
-			kollektioner = await data.json();
-			categories = await catdata.json();
-			console.log(categories);
-			visKollektioner();
-			opretKnapper();
-		}
 
-		function opretKnapper() {
-			categories.forEach(cat => {
-				document.querySelector("#filtrering").innerHTML += `<button class="filter" data-kollektion="${cat.id}">${cat.name}</button>`
+    //Når alt content på siden er loaded sætter vi functionen start igang og henter json//
+    document.addEventListener("DOMContentLoaded", start);
 
-			})
+    function start() {
+        getJson();
+    }
 
-			addEventListenersToButtons();
-		}
+    async function getJson() {
+        const data = await fetch(dburl);
+        const catdata = await fetch(caturl);
+        kollektioner = await data.json();
+        categories = await catdata.json();
+        console.log(categories);
+        visKollektioner();
+        opretKnapper();
+    }
 
-		function addEventListenersToButtons() {
-			document.querySelectorAll("#filtrering button").forEach(elm => {
-				elm.addEventListener("click", filtrering);
-			})
-		};
+    function opretKnapper() {
+        categories.forEach(cat => {
+            document.querySelector("#filtrering").innerHTML += `<button class="filter" data-kollektion="${cat.id}">${cat.name}</button>`
 
-		function filtrering() {
-			filterKollektion = this.dataset.kollektion;
-			visKollektioner();
-		}
+        })
 
-		//Viser produkterne gennem et forEach loop//
-		function visKollektioner() {
-			let temp = document.querySelector("template");
-			let container = document.querySelector("#kollektion-oversigt");
-			container.innerHTML = "";
-			kollektion.forEach(kollektion => {
-				if (kollektion.categories.includes(parseInt(filterKollektion))) {
-					let klon = temp.cloneNode(true).content;
+        addEventListenersToButtons();
+    }
 
-					klon.querySelector(".titel").textContent = kollektion.title.rendered;
+    function addEventListenersToButtons() {
+        document.querySelectorAll("#filtrering button").forEach(elm => {
+            elm.addEventListener("click", filtrering);
+        })
+    };
 
-					klon.querySelector("img").src = kollektion.billede[0].guid;
+    function filtrering() {
+        filterKollektion = this.dataset.kollektion;
+        visKollektioner;
+    }
 
-					klon.querySelector("article").addEventListener("click", () => {
-						location.href = kollektion.link;
-					})
-					container.appendChild(klon);
-				}
-			})
-		}
+    //Viser produkterne gennem et forEach loop//
+    function visKollektioner() {
+        let temp = document.querySelector("template");
+        let container = document.querySelector("#kollektion-oversigt");
+        container.innerHTML = "";
+        kollektioner.forEach(kollektion => {
+            if (kollektion.categories.includes(parseInt(filterKollektion))) {
+                let klon = temp.cloneNode(true).content;
 
-		getJson();
+                klon.querySelector(".titel").textContent = kollektion.title.rendered;
 
-	</script>
+                klon.querySelector("img").src = kollektion.billede[0].guid;
 
-	<?php
+                klon.querySelector("article").addEventListener("click", () => {
+                    location.href = kollektion.link;
+                })
+                container.appendChild(klon);
+            }
+        })
+    }
+
+    getJson();
+
+</script>
+
+<?php
 get_footer();
