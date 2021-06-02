b8573505a6580546449983b1e24dfc60d8a319ab
KatrinMaria <katrinmaria111@gmail.com>
2021-06-02 11:34:58 +0200

    console log visprodukt

--- wordpress/wp-content/themes/tilu/single-produkt.php
+++ wordpress/wp-content/themes/tilu/single-produkt.php
@@ -12,56 +12,57 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-			<article id="indhold">
+        <article id="indhold">
 
-				<h1></h1>
-
-
-
-				<img src="" alt="" class="billede">
-				<h1 class="titel"></h1>
-				<p class="beskrivelse"></p>
-
-				<button id="knap">Tilbage</button>
-
-			</article>
-		</main>
-		<!-- #site-content -->
-	</div>
-	<!-- #primary -->
+            <h1></h1>
 
 
-	<script>
-		let produkter;
-		let aktuelprodukt = <?php echo get_the_ID() ?>;
 
-		const dbUrl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/produkt?per_page=100" + aktuelprodukt;
-		const container = document.querySelector("#indhold");
+            <img src="" alt="" class="billede">
+            <h1 class="titel"></h1>
+            <p class="beskrivelse"></p>
 
-		document.addEventListener("DOMContentLoaded", getJSON);
+            <button id="knap">Tilbage</button>
 
-		async function getJSON() {
-			const data = await fetch(dbUrl);
-			produkt = await data.json();
-
-			visProdukt();
-		}
-
-		function visProdukt() {
-			document.querySelector(".titel").innerHTML = produkt.title.rendered;
-			document.querySelector(".billede").src = produkt.billede.guid;
-			document.querySelector("#knap").addEventListener("click", tilbageTilProdukter);
-		}
-
-		function tilbageTilProdukter() {
-			history.back();
-		}
-
-	</script>
+        </article>
+    </main>
+    <!-- #site-content -->
+</div>
+<!-- #primary -->
 
 
-	<?php
+<script>
+    let produkter;
+    let aktuelprodukt = <?php echo get_the_ID() ?>;
+
+    const dbUrl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/produkt?per_page=100" + aktuelprodukt;
+    const container = document.querySelector("#indhold");
+
+    document.addEventListener("DOMContentLoaded", getJSON);
+
+    async function getJSON() {
+        const data = await fetch(dbUrl);
+        produkt = await data.json();
+
+        visProdukt();
+    }
+
+    function visProdukt() {
+        console.log(produkt.title.rendered);
+        document.querySelector(".titel").innerHTML = produkt.title.rendered;
+        document.querySelector(".billede").src = produkt.billede.guid;
+        document.querySelector("#knap").addEventListener("click", tilbageTilProdukter);
+    }
+
+    function tilbageTilProdukter() {
+        history.back();
+    }
+
+</script>
+
+
+<?php
 get_footer();
