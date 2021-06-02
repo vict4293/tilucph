563e74b378836ec58153fd49f76b58d1481811cd
Victoria Løkke <victorialoekke@MacBook-Pro-tilhrende-Victoria.local>
2021-06-02 11:50:54 +0200

    console

--- wordpress/wp-content/themes/tilu/single-produkt.php
+++ wordpress/wp-content/themes/tilu/single-produkt.php
@@ -72,6 +72,7 @@ get_header();
 		}
 
 		function visProdukt() {
+			console.log(visProdukt);
 			document.querySelector(".titel").innerHTML = produkt.title.rendered;
 			document.querySelector(".billede").src = produkt.billede.guid;
 			document.querySelector("#knap").addEventListener("click", tilbageTilProdukter);
