40cf851aaecf57862a4ed6e3392c6ec54b2819ab
KatrinMaria <katrinmaria111@gmail.com>
2021-06-02 11:34:58 +0200

    console log visprodukt

--- wordpress/wp-content/themes/tilu/single-produkt.php
+++ wordpress/wp-content/themes/tilu/single-produkt.php
@@ -12,6 +12,7 @@
 get_header();
 ?>
 
+<<<<<<< HEAD
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<section id="singleProdukt"></section>
@@ -57,26 +58,63 @@ get_header();
 		const container = document.querySelector(".indhold");
 
 		const flereTemp = document.querySelector("#fleretemp");
+=======
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-		document.addEventListener("DOMContentLoaded", getJSON);
+        <article id="indhold">
 
-		async function getJSON() {
-			const data = await fetch(dbUrl);
-			produkt = await data.json();
+            <h1></h1>
 
+
+
+            <img src="" alt="" class="billede">
+            <h1 class="titel"></h1>
+            <p class="beskrivelse"></p>
+
+            <button id="knap">Tilbage</button>
+
+        </article>
+    </main>
+    <!-- #site-content -->
+</div>
+<!-- #primary -->
+
+
+<script>
+    let produkter;
+    let aktuelprodukt = <?php echo get_the_ID() ?>;
+
+    const dbUrl = "http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/produkt?per_page=100" + aktuelprodukt;
+    const container = document.querySelector("#indhold");
+>>>>>>> console log visprodukt
+
+    document.addEventListener("DOMContentLoaded", getJSON);
+
+    async function getJSON() {
+        const data = await fetch(dbUrl);
+        produkt = await data.json();
+
+<<<<<<< HEAD
 			let jsonData3 = await fetch("http://victorialoekke.dk/kea/tilu/wordpress/wp-json/wp/v2/produkt?categories=" + mereSomDette);
 			mereSom = await jsonData3.json();
 
 			visProdukt();
 			visMere();
 		}
+=======
+        visProdukt();
+    }
+>>>>>>> console log visprodukt
 
-		function visProdukt() {
-			document.querySelector(".titel").innerHTML = produkt.title.rendered;
-			document.querySelector(".billede").src = produkt.billede.guid;
-			document.querySelector("#knap").addEventListener("click", tilbageTilProdukter);
-		}
+    function visProdukt() {
+        console.log(produkt.title.rendered);
+        document.querySelector(".titel").innerHTML = produkt.title.rendered;
+        document.querySelector(".billede").src = produkt.billede.guid;
+        document.querySelector("#knap").addEventListener("click", tilbageTilProdukter);
+    }
 
+<<<<<<< HEAD
 		function visMere() {
 
 			mereSom.forEach(produkt => {
@@ -100,9 +138,14 @@ get_header();
 		function tilbageTilProdukter() {
 			history.back();
 		}
+=======
+    function tilbageTilProdukter() {
+        history.back();
+    }
+>>>>>>> console log visprodukt
 
-	</script>
+</script>
 
 
-	<?php
+<?php
 get_footer();
